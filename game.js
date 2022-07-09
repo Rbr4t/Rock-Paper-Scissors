45606e5adf45aaa7ebea202e5a7feb2dee422211
Robert <robertkoor0@gmail.com>
2022-07-09 14:39:10 +0300

    game almost finished

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-console.log("TEST!")
 
 function computerPlay() {
     const options = ["rock", "paper", "scissors"];
@@ -33,8 +32,31 @@ function single_round(player, computer) {
     };
     
 }
-let computer = computerPlay()
-console.log(computer)
-let player = "scissors"
-console.log(player)
-console.log(single_round(player, computer))
+function game() {
+    let player_score = 0;
+    let computer_score = 0;
+    for (i=0; i<5; i++) {
+        let player = prompt("Select your weapon: (rock, paper, scissors): ")
+        let computer = computerPlay()
+        console.log(`You chose ${player}`)
+        console.log(`Computer chose ${computer}`)
+        if ((single_round(player, computer).indexOf("You won!") > -1)) {
+            player_score += 1;
+        } else if ((single_round(player, computer).indexOf("You lost!") === -1)){
+            computer_score += 1;
+        };
+        
+        console.log(single_round(player, computer))
+
+        console.log([player_score, computer_score])
+    }
+    if (player_score > computer_score) {
+        console.log(`Player won with ${player_score}!`)
+    } else if (player_score < computer_score) {
+        console.log(`Computer won with ${computer_score}!`)
+    } else {
+        console.log("Tie!")
+    }
+}
+
+game()
