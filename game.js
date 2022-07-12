edb294d3004e6ae0d4289700dd1f2b5f03444ec2
Robert <robertkoor0@gmail.com>
2022-07-12 13:18:41 +0300

    forgot to commit

--- game.js
+++ game.js
@@ -1,74 +1,103 @@
+let computer_score = 0;
+let player_score = 0;
+let gameRunning = true;
 
+// Here we define computerPlay
 function computerPlay() {
-    const options = ["rock", "paper", "scissors"];
-    let rand = Math.floor(Math.random()*3)
-    return (options[rand]);
+  const options = ["rock", "paper", "scissors"];
+  let rand = Math.floor(Math.random() * 3);
+  return options[rand];
 }
 
-
+// Here we define single_round
 function single_round(player, computer) {
-    player = player.toLowerCase();
-    if (player === computer) {
-        return "Tie!"
-    }
-    
-    switch (player != computer) {
-        case (player==="rock" && computer==="paper"):
-            return `You lose! ${computer} beats ${player}`;
-            break;
-        case (player==="rock" && computer==="scissors"):
-            return `You won! ${player} beats ${computer}`;
-            break;
+  player = player.toLowerCase();
+  if (player === computer) {
+    return "Tie!";
+  }
 
-        case (player==="paper" && computer==="rock"):
-            return `You won! ${player} beats ${computer}`;
-            break;
-        case (player==="paper" && computer==="scissors"):
-            return `You lose! ${computer} beats ${player}`;
-            break;
+  switch (player !== computer) {
+    case player === "rock" && computer === "paper":
+      ++computer_score;
+      return `You lose! ${computer} beats ${player}`;
+    case player === "rock" && computer === "scissors":
+      ++player_score;
+      return `You won! ${player} beats ${computer}`;
+    case player === "paper" && computer === "rock":
+      ++player_score;
+      return `You won! ${player} beats ${computer}`;
+    case player === "paper" && computer === "scissors":
+      ++computer_score;
+      return `You lose! ${computer} beats ${player}`;
+    case player === "scissors" && computer === "rock":
+      ++computer_score;
+      return `You lose! ${computer} beats ${player}`;
+    case player === "scissors" && computer === "paper":
+      ++player_score;
+      return `You won! ${player} beats ${computer}`;
 
-        case (player==="scissors" && computer==="rock"):
-            return `You lose! ${computer} beats ${player}`;
-            break;
-        case (player==="scissors" && computer==="paper"):
-            return `You won! ${player} beats ${computer}`;
-            break;
-
-        default: 
-            return "Something went wrong!"
-            break;
-    };
-    
+    default:
+      return "Something went wrong!";
+  }
 }
-function game() {
-    let player_score = 0;
-    let computer_score = 0;
-    for (i=0; i<5; i++) {
-        
-        let player = prompt("Select your weapon: (rock, paper, scissors): ", "")
-        let computer = computerPlay()
-        console.log(`You chose ${player}`)
-        console.log(`Computer chose ${computer}`)
-        if (single_round(player, computer).indexOf("Tie!") > -1 || single_round(player, computer).indexOf("Something went wrong!") > -1) {
-            
-        } else if (single_round(player, computer).indexOf("You won!") > -1) {
-            player_score += 1;
-        } else if (single_round(player, computer).indexOf("You lost!") === -1){
-            computer_score += 1;
-        };
-        
-        console.log(single_round(player, computer))
+function updateScore(computer_score, player_score) {
+  if (computer_score !== 5 || player_score !== 5){
+    
+  
+    const scorePlayer = document.querySelector(".scorePlayer");
+    const scoreComputer = document.querySelector(".scoreComputer");
+    let scoreCpr = `Computer score: ${computer_score}`;
+    let scorePly = `Player score: ${player_score}`;
+    scorePlayer.textContent = scorePly;
+    scoreComputer.textContent = scoreCpr;
+  }
+}
+// Here we define game()
+function game(weapon, computer_score, player_score, text) {
+  const container = document.querySelector(".results");
+  console.log(player_score)
 
-        console.log("")
-    }
-
-    if (player_score > computer_score) {
-        console.log(`Player won with ${player_score}!`)
-    } else if (player_score < computer_score) {
-        console.log(`Computer won with ${computer_score}!`)
-    } else {
-        console.log("Tie!")
-    }
+  if (player_score === 5) {
+    container.textContent = "Player won!";
+    gameRunning = false;
+  } else if (computer_score === 5) {
+    container.textContent = "Computer won!";
+    gameRunning = false;
+  } else {
+    
+    container.textContent = text;
+  }
+  
 }
 
-game()
+// Here we grab the DOM elements and set them as variables
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+
+rock.addEventListener("click", function () {
+  //always add the desired function as a callback one!
+  if (gameRunning) {
+    let text = single_round("rock", computerPlay());
+    game("rock", computer_score, player_score, text);
+    updateScore(computer_score, player_score);
+  };
+});
+// And this one
+paper.addEventListener("click", function () {
+  //like here?
+  if (gameRunning) {
+    let text = single_round("paper", computerPlay());
+    game("paper", computer_score, player_score, text);
+    updateScore(computer_score, player_score);
+  };
+});
+// And this one
+scissors.addEventListener("click", function () {
+  if (gameRunning){
+    let text = single_round("scissors", computerPlay());
+    game("scissors", computer_score, player_score, text);
+    updateScore(computer_score, player_score);
+  };
+});
